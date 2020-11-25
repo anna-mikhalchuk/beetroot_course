8165844f53bf7d7926d1ddb5dccdc219ab532e26
Anna Mikhalchuk <anna.mikhalchuk@onereach.com>
2020-11-25 13:13:56 +0200

    tasks descriptions were added

--- task3.py
+++ task3.py
@@ -1,3 +1,8 @@
+# Write a program, which has two print statements to print the following text:
+# (capital letters “O” and “H”, made from “#” symbols)
+# Pay attention that usage of spaces is forbidden, as well as
+# creating the whole result text string using “”” ”””, use ‘\n’ and ‘\t’ symbols instead.
+
 print("##########", end = '\n')
 print("#", end = '\t\t # \n')
 print("#", end = '\t\t # \n')
